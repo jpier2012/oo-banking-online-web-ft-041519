0af9e589a569d58b0ac1bf7dff5569066f1b5a00
James Pier <jpier2012@gmail.com>
2019-04-23 15:34:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
 
   def execute_transaction
     if self.valid?
-      self.sender.balance -= transfer_amount
+      self.sender.balance -= self.amount
       self.receiver.balance += transfer_amount
       self.status = 'complete'
     else
